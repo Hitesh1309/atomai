c7dbe8588426101b92bc45c7775bd31733a5736b
Hitesh Gupta <81859555+Hitesh1309@users.noreply.github.com>
2024-06-04 16:38:27 +0530

    Update Mat2Spec.py

--- Mat2Spec/Mat2Spec.py
+++ Mat2Spec/Mat2Spec.py
@@ -247,10 +247,10 @@ def compute_c_loss(BX, BY, tau=1):
 
 def compute_alpha_loss(fe_mix_coeff):
     alpha_loss = 0
-    print(fe_mix_coeff.shape)
+    # print(fe_mix_coeff.shape)
     for i in range(fe_mix_coeff.size(0) - 1):
         alpha_loss += torch.sum(torch.abs(fe_mix_coeff[i] - fe_mix_coeff[i + 1]))
-        print(alpha_loss)
+        # print(alpha_loss)
     return alpha_loss
 
 def compute_loss(input_label, output, NORMALIZER, args):
